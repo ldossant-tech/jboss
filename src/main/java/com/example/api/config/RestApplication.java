058a6750da6654018ad77c09644606a2b36fd851
Piassa <ldossant@redhat.com>
2025-09-01 13:20:10 -0300

    fix: Ajuste Compose e Dockerfile

--- src/main/java/com/example/api/config/RestApplication.java
+++ src/main/java/com/example/api/config/RestApplication.java
@@ -4,34 +4,33 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
 @ApplicationPath("/api")
 @OpenAPIDefinition(
-    info = @Info(
-        title = "API de Produtos",
-        version = "1.0.0",
-        description = "API REST para gerenciamento de produtos com JBoss e PostgreSQL"
-    ),
-    servers = {
-        @Server(url = "http://localhost:8080/jboss-api/api", description = "Servidor Local")
-    }
+        info = @Info(
+                title = "API de Produtos",
+                version = "1.0.0",
+                description = "API REST para gerenciamento de produtos com JBoss e PostgreSQL"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080/jboss-api/api", description = "Servidor Local")
+        }
 )
 public class RestApplication extends Application {
-    
+
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> classes = new HashSet<>();
         classes.add(com.example.api.resource.ProdutoResource.class);
-        
-        // Adicionar classes do Swagger
+
+        // Swagger resources
         classes.add(io.swagger.v3.jaxrs2.integration.resources.OpenApiResource.class);
         classes.add(io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource.class);
-        
+
         return classes;
     }
 }
-
